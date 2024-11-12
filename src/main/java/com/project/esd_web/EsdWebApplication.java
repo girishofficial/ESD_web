4fdf3cc45320ce8de5ae0685abf31ca296cb741b
girish <girish445g@gmail.com>
2024-11-12 10:42:30 +0530

    New Repo Created

---
+++ src/main/java/com/project/esd_web/EsdWebApplication.java
@@ -0,0 +1,13 @@
+package com.project.esd_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdWebApplication.class, args);
+    }
+
+}
